e67b50cd8c941d65748d42d0d5dd890c12f52246
mThanushree <92203844+mThanushree@users.noreply.github.com>
2021-10-09 13:27:47 +0530

    Add files via upload

---
+++ Encapsulation/IplT.java
@@ -0,0 +1,33 @@
+public class IplT {
+    private String teamName;
+	private int noOfPlayers;
+	private String captainName;
+	private String color;
+	
+	public String getTeamName() {
+	    return teamName;
+	}
+	public void setTeamName(String teamName) {
+	    this.teamName = teamName;
+	}
+	public int getNoOfPlayers() {
+	    return  noOfPlayers;
+	}
+	public void setNoOfPlayers(int  noOfPlayers) {
+	    this.noOfPlayers = noOfPlayers;
+	}
+	public String getCaptainName() {
+	    return captainName;
+	}
+	public void setCaptainName(String captainName) {
+	    this.captainName = captainName;
+	}
+	public String getColor() {
+	    return color;
+	}
+	public void setColor(String color) {
+	    this.color = color;
+	}
+}
+	
+	

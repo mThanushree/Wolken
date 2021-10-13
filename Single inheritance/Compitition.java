28dd6cf8802c4f6ad152c12b4776c847118cef97
mThanushree <92203844+mThanushree@users.noreply.github.com>
2021-10-13 11:39:38 +0530

    Add files via upload

---
+++ Single inheritance/Compitition.java
@@ -0,0 +1,8 @@
+class Compitition extends Olympics {
+    String type;
+    byte noOfCategories;
+	
+    void toGetAnAward() {
+        System.out.println("number of comp");
+    }	
+}
---
+++ Single inheritance/Olympics.java
@@ -0,0 +1,8 @@
+class Olympics {
+    String name;
+    long contact;
+	
+    void toPlay() {
+	System.out.println("Olympics starts");
+    }
+}

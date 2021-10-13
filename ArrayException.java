1e9ee2a9aa68274d87b62606209a9e56b1970324
mThanushree <92203844+mThanushree@users.noreply.github.com>
2021-10-13 23:49:25 +0530

    Add files via upload

---
+++ ArrayException.java
@@ -0,0 +1,9 @@
+public class ArrayException {
+    public static void main(String []args) {
+	String []names = {"thanu","nik","priya"};
+	for(int i= 0; i <= names.length; i++) {
+	    System.out.println(names[i]);
+	}
+	System.out.println("End of main()");
+    }
+}
---
+++ Try-Catch Method/ArrayException.java
@@ -0,0 +1,13 @@
+public class ArrayException {
+    public static void main(String []args) {
+	String []names = {"thanu","nik","priya"};
+	for(int i= 0; i <= names.length; i++) {
+	try {
+	    System.out.println(names[i]);
+	} catch(ArrayIndexOutOfBoundsException e) {
+	    System.out.println(e.getMessage());
+	}
+	}
+	    System.out.println("End of main()");
+    }
+}

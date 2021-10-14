1b09385e9cd0654e9fdec9c59a390c4a15a01adc
mThanushree <92203844+mThanushree@users.noreply.github.com>
2021-10-14 11:12:32 +0530

    Add files via upload

--- Try-Catch Method/Bound.java
+++ Try-Catch Method/Bound.java
@@ -1,9 +1,13 @@
-public class ArrayException {
+public class Bound {
     public static void main(String []args) {
 	String []names = {"thanu","nik","priya"};
 	for(int i= 0; i <= names.length; i++) {
+	try {
 	    System.out.println(names[i]);
-	}
-	System.out.println("End of main()");
+	} catch(ArrayIndexOutOfBoundsException e) {
+	    System.out.println(e.getMessage());
     }
+    }	
+	 System.out.println("End of main()");
+}
 }

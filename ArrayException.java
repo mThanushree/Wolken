b51822f2114de746c47c08916af4593847a937e8
mThanushree <92203844+mThanushree@users.noreply.github.com>
2021-10-14 11:13:25 +0530

    Delete ArrayException.java

--- ArrayException.java
+++
@@ -1,9 +0,0 @@
-public class ArrayException {
-    public static void main(String []args) {
-	String []names = {"thanu","nik","priya"};
-	for(int i= 0; i <= names.length; i++) {
-	    System.out.println(names[i]);
-	}
-	System.out.println("End of main()");
-    }
-}
